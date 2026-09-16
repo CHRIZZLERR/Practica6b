e70a4fdb0d5fb8f5994272c1d30bb310dcabc0c2
Christopher <chrissan1801@gmail.com>
2026-09-16 09:05:25 -0400

    base de mi primera web con javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Cual es tu nombre");
+let apellido = prompt("Cual es tu apellido");
+let anoNacimiento = Number(prompt("En que ano naciste"));
+let edad = 2026 - anoNacimiento;
+let esMayorEdad = edad > 18 ? "Eres mayor de edad": "Eres menor de edad";
+
+let mensaje = `Hola ${nombre} ${apellido} usted nacio ${anoNacimiento} y tienes ${edad}, y usted es ${esMayorEdad}`;
+
+alert(mensaje);
